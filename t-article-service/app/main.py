5e42cdacd8da7e9b161ff2ca2e6c676bf60ce1b6
Kristiyan Strahilov <55084258+kpuc00@users.noreply.github.com>
2022-06-21 12:54:10 +0200

    connected t-article-service to nginx

--- t-article-service/app/main.py
+++ t-article-service/app/main.py
@@ -4,8 +4,8 @@ from app.api.db import metadata, database, engine
 
 metadata.create_all(engine)
 
-app = FastAPI(openapi_url="/api/articles/openapi.json",
-              docs_url="/api/articles/docs")
+app = FastAPI(openapi_url="/articles/openapi.json",
+              docs_url="/articles/docs")
 
 
 @app.on_event("startup")
@@ -17,4 +17,4 @@ async def startup():
 async def shutdown():
     await database.disconnect()
 
-app.include_router(articles, prefix="/api/articles", tags=["articles"])
+app.include_router(articles, prefix="/articles", tags=["articles"])
